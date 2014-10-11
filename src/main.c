b4783d3e90354ef2ecbb6a13509239b1b966d878
Pimzero <g33k.pim@gmail.com>
2014-10-11 22:23:06 +0200

    [main] flag '-i' working properly

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
-#ifndef NOGUI
-#include "gui/gui.h"
-#endif
 
-#define FLAG_ALREADY_SET    0x01
-#define FLAG_UNDEFINED      0x02
 
 void show_help(void);
 int get_flags(int argc, char *argv[], struct s_flags *flags);
@@ -14,7 +9,9 @@ int print_flag_error(char *flag, int error);
 int main(int argc, char *argv[])
 {
     struct s_flags flags;
+
     flags.gui = 0;
+    flags.filename = 0;
 
     if(argc <= 1)
     {
@@ -51,11 +48,25 @@ int get_flags(int argc, char *argv[], struct s_flags *flags)
     int i;
     for (i = 1; i < argc; i++)
     {
-        if(strcmp(argv[i], "-gui") == 0) {
+        if(strcmp(argv[i], "-gui") == 0)
+        {
             if(flags->gui)
                 return print_flag_error(argv[i], FLAG_ALREADY_SET);
-            (*flags).gui = 1;
-        } else
+            flags->gui = 1;
+        }
+        else if(strcmp(argv[i], "-i") == 0)
+        {
+            if(flags->filename)
+                return print_flag_error(argv[i], FLAG_ALREADY_SET);
+            i++;
+            if(i >= argc)
+                return print_flag_error(argv[i - 1], FLAG_MISSING_ARG);
+            flags->filename = argv[i];
+            if(load_from_file(argv[i]))
+                return print_flag_error(argv[i], FLAG_INVALID_ARG);
+            printf("Image loaded (%s)\n", argv[i]);
+        }
+        else
             return print_flag_error(argv[i], FLAG_UNDEFINED);
     }
     return 0;
@@ -63,10 +74,15 @@ int get_flags(int argc, char *argv[], struct s_flags *flags)
 
 int print_flag_error(char *flag, int error)
 {
-    if(error == FLAG_ALREADY_SET)
-        printf("Error, flag already set : %s\n", flag);
-    if(error == FLAG_UNDEFINED)
-        printf("Error, flag undefined : %s\n", flag);
-    printf("type \"ocrocaml -h\" for help\n");
+    if(error & FLAG_ALREADY_SET)
+        fprintf(stderr, "Error : flag already set : %s\n", flag);
+    if(error & FLAG_UNDEFINED)
+        fprintf(stderr, "Error : flag undefined : %s\n", flag);
+    if(error & FLAG_MISSING_ARG)
+        fprintf(stderr, "Error : missing argument for flag : %s\n", flag);
+    if(error & FLAG_INVALID_ARG)
+        fprintf(stderr, "Error : invalid argument '%s'\n", flag);
+
+    printf("type \"ocrocaml -h\" for help\n\n");
     return 1;
 }
--- src/main.h
+++ src/main.h
@@ -3,9 +3,20 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "picture/loader.h"
+
+#ifndef NOGUI
+#include "gui/gui.h"
+#endif
+
+#define FLAG_ALREADY_SET    0x01
+#define FLAG_UNDEFINED      0x02
+#define FLAG_MISSING_ARG    0x04
+#define FLAG_INVALID_ARG    0x08
 
 struct s_flags {
     char gui;
+    char* filename;
     // Other flags
 };
 
