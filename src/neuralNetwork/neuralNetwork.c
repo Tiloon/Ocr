b19dc3f3dc2a6afde09301aff071c0a073d4ecfd
Baptiste DELHOMMEAU <baptiste.delhommeau.17@gmail.com>
2014-10-14 01:57:45 +0200

    [NN] Fix computing values and others stuffs #3

--- src/neuralNetwork/neuralNetwork.c
+++ src/neuralNetwork/neuralNetwork.c
@@ -12,9 +12,8 @@
 //*************************************//
 //            DEFINE                   //
 //*************************************//
-#define BIAS 0.5 
 
-
+#define BIAS 0
 
 
 
@@ -26,112 +25,124 @@
 
 int main(void)
 {
-  /*THIS IS JUST FOR TESTING*/
-  /*NN with 3 layer, 1 input, 1 hidden 1 output*/
-  /*No Learning for now*/
+    /*THIS IS JUST FOR TESTING*/
+    /*NN with 3 layer, 1 input, 1 hidden 1 output*/
+    /*No Learning for now*/
 
-  Layer Input;
-  Layer Hidden;
-  Layer Output;
+    Layer Input;
+    Layer Hidden;
+    Layer Output;
 
-  initializeLayer(&Input, 3, 3);
-  initializeLayer(&Hidden, 2, 2);
-  initializeLayer(&Output, 1, 1);
+    initializeLayer(&Input, 2, 2);
+    initializeLayer(&Hidden, 4, 4);
+    initializeLayer(&Output, 2, 0);
 
-  Input.Units[0].computedValue = 1;
-  Input.weights[0][0] = 0.5;
-  Input.weights[0][1] = 0.2;
-
-  Input.Units[1].computedValue = 2;
-  Input.weights[1][0] = 0.5;
-  Input.weights[1][1] = 0.3;
-
-  Input.Units[2].computedValue = 3;
-  Input.weights[2][0] = 0.5;
-  Input.weights[2][1] = 0.1;
   
+    Input.Units[0].computedValue = 1;
+    Input.Units[1].computedValue = 0;
   
-  Hidden.weights[0][0] = 0.2;  
-  Hidden.weights[1][0] = 0.1;
+    Input.weights[0][0] = 0.5;
+    Input.weights[0][1] = 0.2;
+    Input.weights[0][2] = 0.5;
+    Input.weights[0][3] = 0.5; 
   
-  
-  computeSum(&Input, &Hidden);
-  computeSum(&Hidden, &Output);
+    Input.weights[1][0] = 0.5;
+    Input.weights[1][1] = 0.2;
+    Input.weights[1][2] = 0.5;
+    Input.weights[1][3] = 0.5;
 
-  //printValues(Hidden.Units[0]);
-  //printValues(Hidden.Units[1]);
-  printf("SOMME : %f, COMPUTE : %f \n", Output.Units[0].sumedValue, Output.Units[0].computedValue);
+     
+    Hidden.weights[0][0] = 0.2;  
+    Hidden.weights[0][1] = 0.1;
+    Hidden.weights[1][0] = 0.2;
+    Hidden.weights[1][1] = 0.1;
+    Hidden.weights[2][0] = 0.2;
+    Hidden.weights[2][1] = 0.1;
+    Hidden.weights[3][0] = 0.5;
+    Hidden.weights[3][1] = 0.2;
 
-  freeLayer(&Input);
-  freeLayer(&Hidden);
-  freeLayer(&Output);
+    
+    computeData(&Input, &Hidden, &Output);
 
-  return 0;
+    printf("SOMME : %Lf, COMPUTE : %Lf \n", Output.Units[0].sumedValue, Output.Units[0].computedValue);
+
+    printf("SOMME : %Lf, COMPUTE : %Lf \n", Output.Units[1].sumedValue, Output.Units[1].computedValue);
+
+
+    freeLayer(&Input);
+    //freeLayer(&Hidden);
+    //freeLayer(&Output);
+
+    return 0;
 }
 
 
 void initializeLayer(Layer *Layer, unsigned pNumberUnits, unsigned pNumberLinks)
 {
-  //Number link for the weights to each neural 
-  //unsigned numberUnits = 0;
-  unsigned numberLinks = 0;
-  Neural Neural;
+    //Number link for the weights to each neural 
+    unsigned numberLinks = 0;
+    Neural Neural;
+    unsigned i;
   
-  //Number of Neuron on the Layer
-  Layer->numberUnits = pNumberUnits;
-  //Set the number of units of the next Layer 
-  Layer->numberLinks = pNumberLinks;
+    //Number of Neuron on the Layer
+    Layer->numberUnits = pNumberUnits;
+    //Set the number of units of the next Layer 
+    Layer->numberLinks = pNumberLinks;
 
-  //Malloc tab of Neural of the Layer
-  Layer->Units = malloc (sizeof (Neural) * pNumberUnits);
+    //Malloc tab of Neural of the Layer
+    Layer->Units = malloc (sizeof (Neural) * pNumberUnits);
+    //Initialize Bias value for each neuron
+    for(i=0; i<pNumberUnits;i++)
+	Layer->Units[i].bias = BIAS;
+    //Malloc matrix for the weights of each Neural
+    Layer->weights = malloc(sizeof(long double) * pNumberUnits);
   
-  //Malloc matrix for the weights of each Neural
-  Layer->weights = malloc(sizeof(double) * pNumberUnits);
-  
-  for(; numberLinks < pNumberLinks; numberLinks++)
+    for(numberLinks = 0; numberLinks < pNumberLinks; numberLinks++)
     {
-      Layer->weights[numberLinks] = malloc(sizeof(double) * pNumberLinks);
+	Layer->weights[numberLinks] = malloc(sizeof(long double) * pNumberLinks);
     }
 }
 
 void freeLayer(Layer *Layer)
 {
-  unsigned i = 0;
-  //Free the dynamic memory due to malloc
-  //i.e All the Neural of the Layer
-  free(Layer->Units);
+    //****************//
+    //TODO
+    //***************//
+
+
+    //Free the weights[][] tab
+    /*for(x = 0; x < Layer->numberUnits; x++)
+      {
+      free(Layer->weights[x]);
+      }*/
   
-  //Free the matrix of the weights
-  for(i = 0; i < Layer->numberUnits; i++)
-    {
-      free(Layer->weights[i]);
-    }
-  free(Layer->weights);
+    //Free the weights[] tab
+    free(Layer->weights);
 }
 
 
 void  computeSum(Layer *Layer1, Layer *Layer2)
 {
-  unsigned l1, l2;
-  //Will sum the sumedValue of each Neural of the Layer
-  for(l2 = 0; l2 < Layer2->numberUnits; l2++)
+    unsigned l1, l2;
+    //Will sum the sumedValue of each Neural of the Layer
+    for(l2 = 0; l2 < Layer2->numberUnits; l2++)
     {
-      //Layer2->Units[l2].sumedValue = BIAS; 
-      for(l1 = 0; l1 < Layer1->numberUnits; l1++)
+	Layer2->Units[l2].sumedValue = Layer2->Units[l2].bias; 
+	//Layer2->Units[l2].sumedValue = BIAS; 
+	for(l1 = 0; l1 < Layer1->numberUnits; l1++)
 	{
-	  //WTFFFFFFF
-	  printf(""
-		   ""
-		   "poids %f\n"
-		 "", Layer1->weights[l1][l2]);
-
-	  Layer2->Units[l2].sumedValue += 
-	    Layer1->weights[l1][l2] * Layer1->Units[l1].computedValue;
+	    Layer2->Units[l2].sumedValue += 
+		Layer1->weights[l1][l2] * Layer1->Units[l1].computedValue;
 	}
-      Layer2->Units[l2].computedValue =  sigmoid(Layer2->Units[l2].sumedValue);
+	Layer2->Units[l2].computedValue =  sigmoid(Layer2->Units[l2].sumedValue);
     }
 }
-double sigmoid(double x)
+long double sigmoid(long double x)
 {
-  return (1 /( 1 + exp(-1 * x)));
+    return (1 / (1 + exp(-1 * x)));
+}
+void computeData(Layer *Input, Layer *Hidden, Layer *Output)
+{
+    computeSum(Input, Hidden);
+    computeSum(Hidden, Output);
 }
--- src/neuralNetwork/neuralNetwork.h
+++ src/neuralNetwork/neuralNetwork.h
@@ -1,8 +1,9 @@
 typedef struct Neural Neural;
 struct Neural
 {
-  double computedValue;
-  double sumedValue;
+  long double computedValue;
+  long double sumedValue;
+  long double bias;
 };
 typedef struct Layer Layer;
 struct Layer
@@ -10,12 +11,14 @@ struct Layer
   unsigned numberUnits;
   unsigned numberLinks;
   Neural *Units;
-  double **weights;
+  long double **weights;
 };
 
 //Compute the sum of each neuron on Layer1 to each neuron on Layer2
+//And also call sigmoid function and update each neuron value
 void computeSum(Layer *Layer1, Layer *Layer2);
-double sigmoid(double x);
+long double sigmoid(long double x);
 //Number links <=> view of the layer view, not of eu each neuron
 void initializeLayer(Layer *Layer, unsigned numberUnits, unsigned numberLinks);
 void freeLayer(Layer *Layer);
+void computeData(Layer *Input, Layer *Hidden, Layer *Output);
