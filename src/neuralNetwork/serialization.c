8f71cfd3e36dcd56812fd651a0a7f0fcb7446caf
Baptiste DELHOMMEAU <baptiste.delhommeau.17@gmail.com>
2014-11-23 21:34:34 +0100

    [NN] serialization #1. Structuring the code

---
+++ src/neuralNetwork/serialization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "serialization.h"
+
+int main(void)
+{
+    FILE *file = NULL;
+    file = fopen("test", "r+");
+    if(file != NULL)
+    {
+	
+    }
+    return 0;
+}
