686d72c826926eb73afe5aecbeb48350bcab247c
Pimzero <g33k.pim@gmail.com>
2014-12-01 18:42:17 +0100

    [Filters] Rotation multichannel

--- src/filters/rot_filter.c
+++ src/filters/rot_filter.c
@@ -5,133 +5,133 @@
 #ifndef M_PI
 #define M_PI 3.1415927
 #endif
- 
+
 #define RAD(A)  (M_PI*((double)(A))/180.0)
 
 void pointrotate(guchar *new, guchar *tab, int i, int j, int teta, int width, int height)
 {
-  double	rotinv[2][2];
-  double        x = 0;
-  double	y = 0;
-  int	        x1 = 0;
-  int		y1 = 0;
-  double      	xm = (width / 2);
-  double	ym = (height / 2);
+    double rotinv[2][2];
+    double        x = 0;
+    double y = 0;
+    int x1 = 0;
+    int y1 = 0;
+    double xm = (width / 2);
+    double ym = (height / 2);
 
-  rotinv[0][0] = cos(teta * M_PI / 180);
-  rotinv[1][1] = cos(teta * M_PI / 180);
-  rotinv[0][1] = -sin(teta * M_PI / 180);
-  rotinv[1][0] = sin(teta * M_PI / 180);
-  x = rotinv[0][0] * (i - xm) + rotinv[0][1] * (j - ym);
-  y = rotinv[1][0] * (i - xm) + rotinv[1][1] * (j - ym);
-  x1 = x;
-  y1 = y;
-  if ((x - x1) > 0.5)
-    x1++;
-  if ((y - y1) > 0.5)
-    y1++;
-  y1 += ym;
-  x1 += xm;
-  if ((x1 >= 0) && (x1 < width) && (y1 >= 0) && (y1 < height))
+    rotinv[0][0] = cos(teta * M_PI / 180);
+    rotinv[1][1] = cos(teta * M_PI / 180);
+    rotinv[0][1] = -sin(teta * M_PI / 180);
+    rotinv[1][0] = sin(teta * M_PI / 180);
+    x = rotinv[0][0] * (i - xm) + rotinv[0][1] * (j - ym);
+    y = rotinv[1][0] * (i - xm) + rotinv[1][1] * (j - ym);
+    x1 = x;
+    y1 = y;
+    if ((x - x1) > 0.5)
+        x1++;
+    if ((y - y1) > 0.5)
+        y1++;
+    y1 += ym;
+    x1 += xm;
+    if ((x1 >= 0) && (x1 < width) && (y1 >= 0) && (y1 < height))
     {
-      new[j*3*height+i] = tab[y1*3*height + x1];
-      new[j*3*height+1+i] = tab[y1*3*height+x1 + 1];
-      new[j*3*height+i+2] = tab[y1*3*height+x1+2];
+        new[j*3*height+i] = tab[y1*3*height + x1];
+        new[j*3*height+1+i] = tab[y1*3*height+x1 + 1];
+        new[j*3*height+i+2] = tab[y1*3*height+x1+2];
     }
 }
 
-guchar			*rotation(guchar	*tab,
-				   int		width,
-				   int		height)
+guchar *rotation(guchar *tab,
+        int width,
+        int height)
 {
-  int			teta = 0;
-  guchar		*new;
-int			i;
-int			j;
-teta = 60;  
-new = tab;
-  for (j = 0; j < height; j++)
+    int teta = 0;
+    guchar *new;
+    int i;
+    int j;
+    teta = 60;
+    new = tab;
+    for (j = 0; j < height; j++)
     {
-     for (i = 0; i < width ; i++)
-	pointrotate(new, tab, i, j, teta, width, height);
+        for (i = 0; i < width ; i++)
+            pointrotate(new, tab, i, j, teta, width, height);
     }
-  return (new);
+    return (new);
 }
 
 int **angle(int **hough, int x, int y, int th)
 {
-  float	r = 0;
-  int	r1 = 0;
-  int   theta = 0;
-float teta = 0;
-  for (theta = 0; theta < 180; theta++)
+    float r = 0;
+    int r1 = 0;
+    int   theta = 0;
+    float teta = 0;
+    for (theta = 0; theta < 180; theta++)
     {
-//printf ("%f ", cos(theta*M_PI/180));
-teta = theta*M_PI/180;     
- r = cos(teta) * x + sin(teta) * y;
-//r = cos (theta)*x+sin(theta)*y;
+        //printf ("%f ", cos(theta*M_PI/180));
+        teta = theta*M_PI/180;
+        r = cos(teta) * x + sin(teta) * y;
+        //r = cos (theta)*x+sin(theta)*y;
 
-r = abs(r);
-//printf ("%f ",r );
-      r1 = r;
-//printf("%d ", r1);
-      if ((r - r1) > 0.5)
-{
-	r1++;
-//printf("%f", r);
-}
+        r = abs(r);
+        //printf ("%f ",r );
+        r1 = r;
+        //printf("%d ", r1);
+        if ((r - r1) > 0.5)
+        {
+            r1++;
+            //printf("%f", r);
+        }
 
-      if ((r1 < th) && (r1 > 0))
-{
-//	if (theta >=90)
-//	hough[r1][theta - 90]++;
- //}
-//else
-	hough[r1][(theta%90)]++;
-//printf (" %d ", r1);
-}
+        if ((r1 < th) && (r1 > 0))
+        {
+            // if (theta >=90)
+            // hough[r1][theta - 90]++;
+            //}
+            //else
+        hough[r1][(theta%90)]++;
+        //printf (" %d ", r1);
     }
-  return (hough);
+}
+return (hough);
 }
 
 static int getmax(int **h, int th)
 {
-  int max = 0;
-  int theta = 0;
-  int i;
-  int j;
+    int max = 0;
+    int theta = 0;
+    int i;
+    int j;
 
-  for (j = 0; j < th; j++)
+    for (j = 0; j < th; j++)
     {
-      for (i = 0; i < 180; i++)
-	{
-//printf ("%d ", h[j][i]);
-	  if (max < h[j][i])
-	    {
-	      max = h[j][i];
-	      theta = i;
-printf("debug : %d\n", max);
-printf("sortie : %d\n", theta);
-printf("sortie2 : %d\n", j);
+        for (i = 0; i < 180; i++)
+        {
+            //printf ("%d ", h[j][i]);
+            if (max < h[j][i])
+            {
+                max = h[j][i];
+                theta = i;
+                printf("debug : %d\n", max);
+                printf("sortie : %d\n", theta);
+                printf("sortie2 : %d\n", j);
+            }
+        }
+        //printf("\n");
     }
-	}
-//printf("\n");
-    }
-  return (theta);
+    return (theta);
 }
 
 int rot_filter(GdkPixbuf *picture, size_t nb_params, char **params)
 {
-int **hough;
+    int **hough;
     float theta = 0;
-theta++;
-int th;
+    theta++;
+    int th;
     int ht, wt;
     int i, j;
     int rowstride;
     int bpp;
-//guchar ** tab;
-guchar *pixel;
+    //guchar ** tab;
+    guchar *pixel;
     unsigned int grayscale = 0;
     if(FLAGS->verbosity && nb_params >= 1)
         printf("Filter parameters : \"%s\"\n", params[0]);
@@ -145,30 +145,30 @@ guchar *pixel;
     //width, and bpp.Also get pointer
     wt = gdk_pixbuf_get_width(picture);
 
-/*pixel = gdk_pixbuf_get_pixels(picture);
-    bpp = 3;
-    rowstride = wt * bpp;
-*/
+    /*pixel = gdk_pixbuf_get_pixels(picture);
+      bpp = 3;
+      rowstride = wt * bpp;
+      */
 
-th = sqrt(ht*ht+wt*wt);
-  hough = calloc(th,sizeof (int *));
-  for (i = 0; i < th; i++)
-    hough[i] = calloc(180, sizeof (int));
-/*for (i = 0; i < th; i++)
-    {
+    th = sqrt(ht*ht+wt*wt);
+    hough = calloc(th,sizeof (int *));
+    for (i = 0; i < th; i++)
+        hough[i] = calloc(180, sizeof (int));
+    /*for (i = 0; i < th; i++)
+      {
       for (j = 0; j < 180; j++)
-	hough[i][j] = 0;
-//printf ("%d ", hough[i][j]);
+      hough[i][j] = 0;
+    //printf ("%d ", hough[i][j]);
     }*/
-/*tab = malloc (sizeof (guchar *) * ht;
-for (i = 0; i < th; i++)
-tab[i]  = malloce (sizeof (guchar) * wt;*/
-//    to pixels.
-pixel = gdk_pixbuf_get_pixels(picture);
-    bpp = 3;
+    /*tab = malloc (sizeof (guchar *) * ht;
+      for (i = 0; i < th; i++)
+      tab[i]  = malloce (sizeof (guchar) * wt;*/
+    //    to pixels.
+    pixel = gdk_pixbuf_get_pixels(picture);
+    bpp = gdk_pixbuf_get_n_channels(picture);
     rowstride = wt * bpp;
 
-for(i = 0; i < ht; i++) //iterate over the height of image.
+    for(i = 0; i < ht; i++) //iterate over the height of image.
     {
         for(j = 0; j < rowstride; j += bpp)
             //read every pixel in the row.skip//bpp bytes
@@ -183,33 +183,35 @@ for(i = 0; i < ht; i++) //iterate over the height of image.
             //pixel[i*rowstride + j+1]=0;
             //pixel[i*rowstride + j+2]=blue
             if(grayscale < 0xFF*3/2)
-{
-//tab[i][j/3];
+            {
+                //tab[i][j/3];
 
-//th = sqrt (i*i+j/3*j/3);
-hough = angle(hough,j/3 , i,  th);
-    grayscale = 0;
-}
+                //th = sqrt (i*i+j/3*j/3);
+                hough = angle(hough,j/3 , i,  th);
+                grayscale = 0;
+            }
             else
                 grayscale = ~0;
 
-	
-        //    pixel[i * rowstride + j] = grayscale;
-          //  pixel[i * rowstride + j + 1] = grayscale;
+
+            //    pixel[i * rowstride + j] = grayscale;
+            //  pixel[i * rowstride + j + 1] = grayscale;
             //pixel[i * rowstride + j + 2] = grayscale;
         }
     }
-theta = getmax(hough,th);
-//pixel = rotation(pixel, wt, ht);
-return theta;
+    theta = getmax(hough,th);
+
+    printf("Detected rotation : %f", theta);
+    //pixel = rotation(pixel, wt, ht);
+    return theta;
 }
-    /*for(i = 0; i < ht; i++) //iterate over the height of image.
-    {
-        for(j = 0; j < rowstride; j += bpp)
-            //read every pixel in the row.skip//bpp bytes
-        {
+/*for(i = 0; i < ht; i++) //iterate over the height of image.
+  {
+  for(j = 0; j < rowstride; j += bpp)
+//read every pixel in the row.skip//bpp bytes
+{
 
 if ((pixel[i*rowstride+j] < 90) && (pixel[i*rowstride+j+1]  < 90) && (pixel[i*rowstride+j+2]  < 90))
-	    {
-	      hough = angle(hough, i, j, th);
-	    }*/
+{
+hough = angle(hough, i, j, th);
+}*/
