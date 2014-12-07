d1a39d58d6303643b5d2e23419a28c3c00d76ebc
lebraz_g <lebraz_g@epita.fr>
2014-12-07 15:21:01 +0100

    ça compile

--- src/filters/sample_filter.c
+++ src/filters/sample_filter.c
@@ -11,121 +11,147 @@
 
 
 
-
-
-void pointrotate(guchar *new, guchar *tab, int i, int j, int teta, int height, int width, int bpp)
+static int	getmax(int	**hough,
+		       int	rmax)
 {
-// i = width = x
-// j = height = y
-int z = i* bpp;
-int v = j *bpp*width;
-  float         rotinv[2][2];
-  float        x = 0;
-  float         y = 0;
-  int           x1 = 0;
-  int           y1 = 0;
-  float         xm = (width) / 2.0;
-  float         ym = (height) / 2.0;
-  rotinv[0][0] = cos(RAD(teta));
-  rotinv[1][1] = cos(RAD(teta));
-  rotinv[0][1] = -sin(RAD(teta));
-  rotinv[1][0] = sin(RAD(teta));
-x= rotinv[0][0] * (i-xm) + rotinv[1][0] * (j-ym);
-y = rotinv[0][1] * (i-xm) + rotinv[1][1] * (j-ym);
-//printf ("xf = %f, yf = %f \n", x, y);
-//printf(" %d et %d et %d \n", tab[42], tab[43], tab[44]);
- // x1 = x;
- // y1 = y;
-//  if ((x - x1) > 0.5)
-  //  x1++;
- // if ((y - y1) > 0.5)
-   // y1++;
-  y += ym ;
-  x += xm ;
-x1 = x;
-y1 = y;
+  int		max = 0;
+  int		teta = 0;
+  int		i;
+  int		j;
+
+  for (j = 0; j < rmax; j++)
+    {
+      for (i = 0; i < 180; i++)
+	{
+	  if (max < hough[j][i])
+	    {
+	      max = hough[j][i];
+	      teta = i;
+	    }
+	}
+    }
+  return (teta);
+}
+
+int	**angle(int	**hough,
+		int	x,
+		int	y,
+		int	rmax)
+{
+  float	r = 0;
+  int	r1 = 0;
+  int   teta = 0;
+
+  for (teta = 0; teta < 180; teta++)
+    {
+      r = cos(teta) * x + sin(teta * M_PI / 180) * y;
+      r1 = r;
+      if ((r - r1) > 0.5)
+	r1++;
+      if ((r1 < rmax) && (r1 > 0))
+	hough[r1][teta]++;
+    }
+  return (hough);
+}
+
+
+float		detectangle(int		**tab,
+			    int		width,
+			    int		height)
+{
+  int		i;
+  int		j;
+  int		rmax = 0;
+  int		**hough;
+  float		teta = 0;
+
+  rmax = sqrt(height * height + width * width);
+  hough = malloc(sizeof (int *) * rmax);
+  for (i = 0; i < rmax; i++)
+    hough[i] = malloc(sizeof (int) * 180);
+  for (i = 0; i < rmax; i++)
+    {
+      for (j = 0; j < 45; j++)
+	hough[i][j] = 0;
+    }
+  for (j = 0; j < height; j++)
+    {
+      for (i = 0; i < width; i++)
+	{
+	  if ((tab[j][i] < 90))
+	    {
+	      hough = angle(hough, i, j, rmax);
+	    }
+	}
+    }
+  printf("end get angle\n");
+  teta = getmax(hough, rmax);
+  return (teta);
+}
+void		pointrotate(int		**new,
+			    int		**tab,
+			    int		i,
+			    int		j,
+			    float		teta,
+			    int		width,
+			    int		height)
+{
+  double	rotinv[2][2];
+  double        x = 0;
+  double	y = 0;
+  int	        x1 = 0;
+  int		y1 = 0;
+  double      	xm = (width / 2);
+  double	ym = (height / 2);
+
+  rotinv[0][0] = cos(teta * M_PI / 180);
+  rotinv[1][1] = cos(teta * M_PI / 180);
+  rotinv[0][1] = -sin(teta * M_PI / 180);
+  rotinv[1][0] = sin(teta * M_PI / 180);
+
+  x = rotinv[0][0] * (i - xm) + rotinv[0][1] * (j - ym);
+  y = rotinv[1][0] * (i - xm) + rotinv[1][1] * (j - ym);
   x1 = x;
   y1 = y;
   if ((x - x1) > 0.5)
     x1++;
   if ((y - y1) > 0.5)
     y1++;
-//printf ("h = %d, w = %d \n", height, width);
-//printf ("xm = %f, ym = %f \n", xm, ym);
-//printf ("xf = %f, yf = %f \n", x, y);
-//printf ("x = %d, y = %d \n", x1, y1);
-  if (/*(x1 >= 0) && (x1 < width) && (y1 >= 0) && (y1 < height) &&*/ (y1*bpp*width+x1*bpp+bpp-1 <= height*bpp*width) )
+  y1 += ym;
+  x1 += xm;
+  if ((x1 >= 0) && (x1 < width) && (y1 >= 0) && (y1 < height))
     {
-
-for (int b = 0; b < bpp; b++)
-{
-//printf ("%d + %d \n", y1, x1);
-//printf ("%d et %d\n", v+z+b, y1*bpp+b+x1*bpp*height);
-//printf("%d\n", tab[y1*bpp+b+x1*bpp*height]);      
-//new[v+z+b] = tab[y1*bpp*width + b + x1*bpp/*y1*bpp+x1*bpp*height*/];
-      //tab[y1*bpp*width+b+x1*bpp] = 255;
-new[v+z+b] = tab[y1*bpp*width+x1*bpp + b/*y1*bpp+x1*bpp*height+1*/];
-      //new[v+z+2] = tab[y1*width*bpp+x1*bpp+2/*y1*bpp+x1*bpp*height+2*/];
-      //new[y1*bpp*width + x1*bpp+b] = tab[v+z+b/*y1*bpp+x1*bpp*height*/];
-      //new[y1*bpp*width + x1*bpp+1] = tab[v+z + 1/*y1*bpp+x1*bpp*height+1*/];
-      //new[y1*bpp*width + x1*bpp+2] = tab[v+z+2/*y1*bpp+x1*bpp*height+2*/];
-}
-}
-//else
-//{
-//for (int b = 0; b < bpp; b++)
-//{
-
-//printf ("%d et %d\n", v+z+b, y1*bpp*width+b+x1*bpp);
-//printf("%d\n", tab[y1*bpp*width+b+x1*bpp]);
-
-//}
-//for(int b=0; b < bpp; b++)
-//{
-//new[v+z+b] = 255;
-//}
-//}
-
-//new[j+i] = 0;
-//new[j+i+1] = 0;
-//new[j+i+2] = 0;
-
-}
-
-void rotation(guchar    *tab,
-                                   int          width,
-                                   int          height, int bpp)
-{
-printf(" %d et %d et %d \n", tab[42], tab[43], tab[44]);
-printf("h : %d w : %d \n", height, width);
-  int                   teta;
-  guchar                *new;
-int                     i;
-int                     j;
-teta =90;
-new = calloc(width * height * bpp,sizeof(guchar));
-for(int a = 0; a < width*height*bpp; a++)
-  { new[a] = 0;
-//printf("%d \n", tab[a]);
-}
-printf ("%d ", width*height*bpp );
-for (j = 0; j < width; j++)
-  {
-   for (i = 0; i < height ; i++)
-//printf ("%d  ", width*bpp*height);
-
-        pointrotate(new, tab, i, j, teta, height, width, bpp);
+      new[j][i] = tab[y1][x1];
     }
-for(int z = 0; z < width*height*bpp; z++)
-{
-//printf("%d \n", new[z]);
-   tab[z] = new[z];
 }
-//for (int a = 0; a < width*height*bpp; a++)
-//tab[a] = 0;
 
-//  return (0);
+void			rotation(int		**tab,
+				   int		width,
+				   int		height)
+{
+  float			teta = 0;
+  int			**new;
+  int			i;
+  int			j;
+
+  teta = detectangle(tab, width, height);
+  printf("teta: %f\n", teta);
+  new = malloc(sizeof (int *) * height);
+  for (i = 0; i < height; i++)
+    new[i] = calloc(sizeof (int), width);
+  for (j = 0; j < height; j++)
+    {
+      for (i = 0; i < width; i++)
+	pointrotate(new, tab, i, j, teta, width, height);
+    }
+
+for (j = 0; j < height; j++)
+    {
+      for (i = 0; i < width; i++)
+	tab[j][i] = new[j][i];   
 }
+}
+
 
 
 
@@ -137,7 +163,7 @@ int sample_filter(GdkPixbuf *picture, size_t nb_params, char **params)
     int bpp;
     unsigned int grayscale;
     guchar *pixel;
-
+int **tab;
     if(FLAGS->verbosity && nb_params >= 1)
         printf("Filter parameters : \"%s\"\n", params[0]);
 
@@ -154,7 +180,11 @@ int sample_filter(GdkPixbuf *picture, size_t nb_params, char **params)
     bpp = gdk_pixbuf_get_n_channels(picture);
 printf ("%d", bpp);
     rowstride = wt * bpp;
-    for(i = 0; i < ht; i++) //iterate over the height of image.
+    
+tab = malloc(sizeof (int *) * ht);
+  for (i = 0; i < ht; i++)
+    tab[i] = calloc(sizeof (int), wt);
+for(i = 0; i < ht; i++) //iterate over the height of image.
     {
         for(j = 0; j < rowstride; j += bpp)
             //read every pixel in the row.skip//bpp bytes
@@ -172,13 +202,27 @@ printf ("%d", bpp);
                 grayscale = 0;
             else
                 grayscale = ~0;
+tab[i][j/3] = grayscale;
 
             pixel[i * rowstride + j] = grayscale;
             pixel[i * rowstride + j + 1] = grayscale;
             pixel[i * rowstride + j + 2] = grayscale;
         }
     }
-rotation(pixel, wt, ht, bpp);
+rotation(tab, wt, ht);
+for(i = 0; i < ht; i++) //iterate over the height of image.
+    {
+        for(j = 0; j < rowstride; j += bpp)
+            //read every pixel in the row.skip//bpp bytes
+        {
+pixel[i * rowstride + j] = tab[i][j/3];
+            pixel[i * rowstride + j + 1] = tab[i][j/3];
+            pixel[i * rowstride + j + 2] = tab[i][j/3];
+        }
+    }
+
+
+
 printf("%d", pixel[0]);
     return 0;
 }
